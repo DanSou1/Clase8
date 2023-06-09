ae36249ca47d3db455927c22398979041d1c117b
DanSou1 <125513682+DanSou1@users.noreply.github.com>
2023-06-08 23:34:32 -0500

    Router y Middelware
    Se habló sobre control de procesos en los path através de funciones middelware donde se usa next() para el siguiente o las de error con err son 4 parametros para error, se subio el primer index para subir objetos en formato json creando servidor estatico(que nunca cambia)

--- src/app.js
+++ src/app.js
@@ -2,9 +2,10 @@ import express from "express";
 import { petsRouter} from './routers/pets.router.js';
 import { usersRouter} from './routers/users.router.js';
 const app = express();
+app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use('/api/users', usersRouter);
 app.use('/api/pets', petsRouter);
 app.listen(8080, ()=>{console.log('escuchando puerto 8080')});
-//Min 28
+//Min 01:40:00
