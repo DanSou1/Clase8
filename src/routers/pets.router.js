11a8f12e3aee0cf56bdd81a309afb98a9eb4eca2
DanSou1 <125513682+DanSou1@users.noreply.github.com>
2023-06-08 23:42:19 -0500

    Control
    Conrtol con middleware

--- src/routers/pets.router.js
+++ src/routers/pets.router.js
@@ -1,6 +1,13 @@
 import { Router } from "express";
 const pets = [];
 const petsRouter = Router();
+//punto de control con middelware
+petsRouter.use((req,res,next)=>{
+    if (req.body.tipo === "Perro") {
+        res.status(403).send('No se admiten perros')
+    }
+    next();
+})
 
 petsRouter.get('/',(req,res)=>{
     res.send(pets);
